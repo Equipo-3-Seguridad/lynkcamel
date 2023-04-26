f663235f6e1f110863d86fdae26fd7fc6ad85e9d
David1A63 <despanime@gmail.com>
2023-04-26 05:07:05 -0600

    Subiendo los archivos

---
+++ resources/views/inicio/index.blade.php
@@ -0,0 +1,10 @@
+<!--Se llama a la plantilla principal ya creada-->
+@extends('plantillas.portada')
+
+@section('contenidoPrincipal')
+<div class="">
+    <h3 class="text-dark fw-bold m">Bienvenidos a Lynk Camel</h3>
+    <p>Camel es un sitio web que permite a los usuarios que estan interesados en buscar empleo tanto como profesionistas asi como de oficio, esto conecta con usuarios con empleadores con el objetivo de que consigas un empleo digno de sus habilidades o talentos. </p>
+    <a href="" class="btn btn-success" style="margin-left: 1%;">Empezar</a>
+</div>
+@endsection
